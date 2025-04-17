04ba80e5495915dfa8a47000895fb79d2dc69611
NeruXV <a2n2r@yandex.ru>
2025-04-17 13:21:09 +0500

    1_6_1
    Feat: added ❌ for unpacked items using conditional operator

--- src/1/1_6_1/App.tsx
+++ src/1/1_6_1/App.tsx
@@ -6,7 +6,7 @@
 function Item({ name, isPacked }: { name: string; isPacked: boolean }) {
   return (
       <li className="item">
-          {name} {isPacked && '✔'}
+          {name} {isPacked ? '✔' : '❌'}
       </li>
   );
 }
@@ -17,14 +17,8 @@ export default function PackingList() {
           <h1>Sally Ride's Packing List</h1>
           <ul>
               <Item isPacked={true} name="Space suit" />
-              <Item
-                  isPacked={true}
-                  name="Helmet with a golden leaf"
-              />
-              <Item
-                  isPacked={false}
-                  name="Photo of Tam"
-              />
+              <Item isPacked={true} name="Helmet with a golden leaf" />
+              <Item isPacked={false} name="Photo of Tam" />
           </ul>
       </section>
   );
