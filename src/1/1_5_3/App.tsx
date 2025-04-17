fbba3e4b9b8cf9d54fd9468c4e43240df16273db
NeruXV <a2n2r@yandex.ru>
2025-04-17 13:12:57 +0500

    1_5_3
    Refactor: extract Card component with children prop

--- src/1/1_5_3/App.tsx
+++ src/1/1_5_3/App.tsx
@@ -3,27 +3,27 @@
   Извлеките компонент Card из приведенной ниже разметки и используйте параметр children для передачи ему различных JSX.
 */
 
+import Card from "./Card"
+
 export default function Profile() {
   return (
     <div>
-      <div className="card">
-        <div className="card-content">
-          <h1>Photo</h1>
-          <img
-            className="avatar"
-            src="OKS67lhm.jpg"
-            alt="Aklilu Lemma"
-            width={70}
-            height={70}
-          />
-        </div>
-      </div>
-      <div className="card">
-        <div className="card-content">
-          <h1>About</h1>
-          <p>Aklilu Lemma was a distinguished Ethiopian scientist who discovered a natural treatment to schistosomiasis.</p>
-        </div>
-      </div>
+      <Card>
+        <h1>Photo</h1>
+        <img
+          className="avatar"
+          src="OKS67lhm.jpg"
+          alt="Aklilu Lemma"
+          width={70}
+          height={70}
+        />
+      </Card>
+      <Card>
+        <h1>About</h1>
+        <p>
+          Aklilu Lemma was a distinguished Ethiopian scientist who discovered a natural treatment to schistosomiasis.
+        </p>
+      </Card>
     </div>
   );
 }
---
+++ src/1/1_5_3/Card.tsx
@@ -0,0 +1,9 @@
+export default function Card({ children }: { children: React.ReactNode }) {
+    return (
+      <div className="card">
+        <div className="card-content">
+          {children}
+        </div>
+      </div>
+    );
+  }
