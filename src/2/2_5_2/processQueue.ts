595a0bd65da16305ce5c9db9cddf4938afe53aa0
NeruXV <a2n2r@yandex.ru>
2025-04-19 12:59:16 +0500

    2_5_2
    Feat: implement getFinalState to process state queue

--- src/2/2_5_2/processQueue.ts
+++ src/2/2_5_2/processQueue.ts
@@ -2,8 +2,11 @@ import { Que } from "./App";
 
 export function getFinalState(baseState: number, queue: Que[]) {
     let finalState = baseState;
-
-    // TODO: do something with the queue...
-
+    for (const item of queue) {
+        if (typeof item === 'function')
+            finalState = item(finalState);
+        else
+            finalState = item;
+    }
     return finalState;
 }
