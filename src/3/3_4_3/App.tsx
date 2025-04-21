8bb3f0bfb906ccd0dac4ea36cbb6f8e40e0dcffc
NeruXV <a2n2r@yandex.ru>
2025-04-22 00:04:48 +0500

    3_4_3
    Fix: reset form state when selected contact changes using key prop

--- src/3/3_4_3/App.tsx
+++ src/3/3_4_3/App.tsx
@@ -42,6 +42,7 @@ export default function ContactManager() {
       />
       <hr />
       <EditContact
+        key={selectedContact.id}
         initialData={selectedContact}
         onSave={handleSave}
       />
