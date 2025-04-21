eb405db2086a49b0dca7a820643a9461fb93bb50
NeruXV <a2n2r@yandex.ru>
2025-04-21 23:40:56 +0500

    3_4_2
    Fix: swap form field values when order is reversed

--- src/3/3_4_2/App.tsx
+++ src/3/3_4_2/App.tsx
@@ -9,7 +9,10 @@ import { useState } from 'react';
 
 export default function App() {
   const [reverse, setReverse] = useState(false);
-  let checkbox = (
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+
+  const checkbox = (
     <label>
       <input
         type="checkbox"
@@ -19,35 +22,42 @@ export default function App() {
       Reverse order
     </label>
   );
-  if (reverse) {
-    return (
-      <>
-        <Field label="Last name" /> 
-        <Field label="First name" />
-        {checkbox}
-      </>
-    );
-  } else {
-    return (
-      <>
-        <Field label="First name" /> 
-        <Field label="Last name" />
-        {checkbox}
-      </>
-    );    
-  }
+
+  return (
+    <>
+      {reverse ? (
+        <>
+          <Field label="Last name" value={lastName} onChange={setLastName} />
+          <Field label="First name" value={firstName} onChange={setFirstName} />
+        </>
+      ) : (
+        <>
+          <Field label="First name" value={firstName} onChange={setFirstName} />
+          <Field label="Last name" value={lastName} onChange={setLastName} />
+        </>
+      )}
+      {checkbox}
+    </>
+  );
 }
 
-function Field({ label }: { label: string }) {
-  const [text, setText] = useState('');
+function Field({
+  label,
+  value,
+  onChange,
+}: {
+  label: string;
+  value: string;
+  onChange: (val: string) => void;
+}) {
   return (
     <label>
       {label}:{' '}
       <input
         type="text"
-        value={text}
+        value={value}
         placeholder={label}
-        onChange={e => setText(e.target.value)}
+        onChange={e => onChange(e.target.value)}
       />
     </label>
   );
