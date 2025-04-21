97b41fe0be3d466c0230741abbc6ba9f1527ec07
NeruXV <a2n2r@yandex.ru>
2025-04-21 13:26:23 +0500

    3_1_1
    Feat: toggle CSS classes on picture and background click

--- src/3/3_1_1/App.tsx
+++ src/3/3_1_1/App.tsx
@@ -5,13 +5,34 @@
     Визуально вы должны увидеть, что щелчок на изображении удаляет фиолетовый фон и выделяет границу изображения. Щелчок за пределами изображения выделяет фон, но убирает выделение границы изображения.
 */
 
+import { useState } from 'react';
+
 export default function Picture() {
+    const [isPictureActive, setIsPictureActive] = useState(false);
+
+    function handleBackgroundClick() {
+        setIsPictureActive(false);
+    }
+
+    function handlePictureClick(e: React.MouseEvent) {
+        e.stopPropagation(); // предотвращает всплытие события на div
+        setIsPictureActive(true);
+    }
+
     return (
-        <div className="background background--active">
+        <div
+            className={
+                'background' + (isPictureActive ? '' : ' background--active')
+            }
+            onClick={handleBackgroundClick}
+        >
             <img
-                className="picture"
+                className={
+                    'picture' + (isPictureActive ? ' picture--active' : '')
+                }
                 alt="Rainbow houses in Kampung Pelangi, Indonesia"
                 src="/5qwVYb1.jpg"
+                onClick={handlePictureClick}
             />
         </div>
     );
