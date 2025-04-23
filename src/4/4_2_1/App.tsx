2180f8767b1f75c417022447dc66ab31f7eae2fd
NeruXV <a2n2r@yandex.ru>
2025-04-23 11:32:13 +0500

    4_2_1
    Fix video playback control with play/pause button and sync with media events

--- src/4/4_2_1/App.tsx
+++ src/4/4_2_1/App.tsx
@@ -9,10 +9,16 @@ import { useState, useRef } from 'react';
 
 export default function VideoPlayer() {
   const [isPlaying, setIsPlaying] = useState(false);
+  const videoRef = useRef<HTMLVideoElement>(null);
 
   function handleClick() {
-    const nextIsPlaying = !isPlaying;
-    setIsPlaying(nextIsPlaying);
+    const video = videoRef.current;
+    if (video) {
+      if (isPlaying)
+        video.pause();
+      else
+        video.play();
+    }
   }
 
   return (
@@ -20,12 +26,18 @@ export default function VideoPlayer() {
       <button onClick={handleClick}>
         {isPlaying ? 'Pause' : 'Play'}
       </button>
-      <video width="250">
+      <video
+        width="250"
+        ref={videoRef}
+        onPlay={() => setIsPlaying(true)}
+        onPause={() => setIsPlaying(false)}
+        controls
+      >
         <source
           src="flower.mp4"
           type="video/mp4"
         />
       </video>
     </>
-  )
+  );
 }
